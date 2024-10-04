c56908d6b99780d6354b3eece40df4965ce77335
Rajendra Bisht <rajendrasinghbisht7060@gmail.com>
2024-10-04 20:57:36 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,40 @@
 const { Router } = require("express");
-const { Module } = require("module");
 const router = Router();
 const User = require("../models/user");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
 const path = require("path");
-const fs = require("fs");
 require("dotenv").config();
+const fs = require("fs");
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-//changes
-const mongoURI = process.env.MONGO_URL;
+// Set up multer for file upload
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads1/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.get("/signup", (req, res) => {
   return res.render("signup");
 });
 
-router.post("/signup", async (req, res) => {
-  const { username, password, profileurlBase64 } = req.body;
+router.post("/signup", upload.single("profileurl"), async (req, res) => {
+  const { username, password } = req.body;
+
   try {
     const user = await User.findOne({ username });
     if (user) {
@@ -23,11 +43,21 @@ router.post("/signup", async (req, res) => {
       });
     }
 
-    await User.create({
+    // Upload image to Cloudinary
+    const profileImage = req.file.path;
+    const result = await cloudinary.uploader.upload(profileImage, {
+      folder: "users",
+    });
+
+    // Store the Cloudinary URL in the database
+    const newUser = await User.create({
       username,
       password,
-      profileurl: profileurlBase64,
+      profileurl: result.secure_url, // Store the Cloudinary URL here
     });
+
+    // Delete the uploaded file from the local system
+    fs.unlinkSync(profileImage);
 
     return res.render("signin", {
       username: username,
